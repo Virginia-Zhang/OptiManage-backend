c6148f6b1da4654ce533edc19d62c03138385aac
virginia <sakurayi0314@qq.com>
2024-10-21 23:40:47 +0800

    chores: merge login branch into master

--- src/main/java/com/virginia/service/impl/LoginServiceImpl.java
+++ src/main/java/com/virginia/service/impl/LoginServiceImpl.java
@@ -9,7 +9,6 @@ import com.virginia.utils.JWTUtils;
 import com.virginia.utils.RedisUtils;
 import jakarta.annotation.Resource;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
